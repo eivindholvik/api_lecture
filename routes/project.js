208946decd933886b10bb17734ea347e19e3dd9f
eivindholvik <eivind.holvik@noroff.no>
2023-02-01 09:14:53 +0100

    add update

--- routes/project.js
+++ routes/project.js
@@ -2,12 +2,21 @@ const express = require("express");
 const router = express.Router();
 const project = require("../services/project");
 
-router.get("/", async function(req, res, next) {
+router.get("/", async function (req, res, next) {
   try {
     res.json(await project.getMultriple(req.query.page));
   } catch (e) {
     console.error(`Error while getting projects ${e}`);
     next(e);
+  }
+});
+
+router.post("/", async function (req, res, next) {
+  try {
+    res.json(await project.create(req.body));
+  } catch (e) {
+    console.error("error while creating project:", e.message);
+    next(e)
   }
 });
 
--- services/project.js
+++ services/project.js
@@ -2,19 +2,40 @@ const db = require("./db");
 const helper = require("../helper");
 const config = require("../config");
 
-async function getMultriple(page = 1){
+async function getMultriple(page = 1) {
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
-    `SELECT * FROM projects LIMIT ${offset},${config.listPerPage}` 
+    `SELECT * FROM projects LIMIT ${offset},${config.listPerPage}`
   );
   const data = helper.emptyOrRows(rows);
-  const meta = {page};
+  const meta = { page };
 
   return {
     data, meta
   }
 }
 
+async function create(project) {
+  // const result = await db.query(`INSERT INTO 'projects'
+  // (title, github_link, live_link, description, image_url, achievements)
+  // VALUES
+  // (${project.title}, ${project.githublink}, ${project.livelink}, ${project.description}, ${project.imageurl}, ${project.achievements})`);
+
+  const result = await db.query(`SELECT 'projects'
+  (title, github_link, live_link, description, image_url, achievements)
+  VALUES
+  (${project.title}, ${project.githublink}, ${project.livelink}, ${project.description}, ${project.imageurl}, ${project.achievements})`);
+
+  let message = `Error in creating project`;
+
+  if (result.affectedRows) {
+    message = "Created project successfully";
+  }
+
+  return message;
+}
+
 module.exports = {
-  getMultriple
+  getMultriple,
+  create
 }
